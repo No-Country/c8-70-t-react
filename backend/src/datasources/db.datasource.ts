4b3c9ec7ae4cb1ab586895770d9d9eb7109f6c2c
Noelia Martínez <noeowo384@gmail.com>
2022-11-16 01:05:27 -0400

    added data source

---
+++ backend/src/datasources/db.datasource.ts
@@ -0,0 +1,28 @@
+import {inject, lifeCycleObserver, LifeCycleObserver} from '@loopback/core';
+import {juggler} from '@loopback/repository';
+
+const config = {
+  name: 'db',
+  connector: 'postgresql',
+  url: '',
+  host: '',
+  port: 5432,
+  user: '',
+  password: '',
+  database: '',
+  ssl: false,
+};
+
+@lifeCycleObserver('datasource')
+export class DbDataSource extends juggler.DataSource
+  implements LifeCycleObserver {
+  static dataSourceName = 'db';
+  static readonly defaultConfig = config;
+
+  constructor(
+    @inject('datasources.config.db', {optional: true})
+    dsConfig: object = config,
+  ) {
+    super(dsConfig);
+  }
+}
