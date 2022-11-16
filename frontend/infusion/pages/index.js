61bb42b8260d0fca303e39111f32da5b19b5b531
agtelo <alantelo1987@gmail.com>
2022-11-16 16:58:39 -0300

    add layout

--- frontend/infusion/pages/index.js
+++ frontend/infusion/pages/index.js
@@ -1,25 +1,9 @@
 import Head from "next/head";
 import Image from "next/image";
+
 import styles from "../styles/Home.module.css";
 import Link from "next/link";
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <ul>
-        <li>
-          <Link href="/">Home</Link>
-        </li>
-        <li>
-          <Link href="/Login">Login</Link>
-        </li>
-        <li>
-          <Link href="/Register">Register</Link>
-        </li>
-        <li>
-          <Link href="/Detail">Detail</Link>
-        </li>
-      </ul>
-    </div>
-  );
+  return <h1>Home</h1>;
 }
