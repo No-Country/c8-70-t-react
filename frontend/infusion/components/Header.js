a9169808c99c1173fe4c2ba79ede31dba2ae4ebc
agtelo <alantelo1987@gmail.com>
2022-11-23 00:07:51 -0300

    add cartMenu component

--- frontend/infusion/components/Header.js
+++ frontend/infusion/components/Header.js
@@ -3,6 +3,7 @@ import { useRouter } from "next/router";
 import Link from "next/link";
 import styles from "../styles/Header.module.css";
 import ProductCounter from "./ProductCounter";
+import CartMenu from "./CartMenu";
 
 export default function Header() {
   const router = useRouter();
@@ -152,100 +153,7 @@ export default function Header() {
           </div>
         </nav>
       </header>
-
-      <div
-        className="offcanvas offcanvas-end border-5"
-        style={{ border: "3px black solid" }}
-        tabindex="-1"
-        id="offcanvasRight"
-        aria-labelledby="offcanvasRightLabel"
-      >
-        <div className="offcanvas-header ">
-          <h5 className="offcanvas-title" id="offcanvasRightLabel">
-            Mi compra
-          </h5>
-          <button
-            type="button"
-            className="btn-close"
-            data-bs-dismiss="offcanvas"
-            aria-label="Close"
-          ></button>
-        </div>
-        <hr
-          className="bg-black opacity-100"
-          style={{ border: "1px black solid" }}
-        ></hr>
-        <div className="offcanvas-body scroll">
-          <div className="d-flex ">
-            <div className="w-50">
-              <img src="/home-product-1.png" width={150}></img>
-            </div>
-            <div className="d-flex flex-column">
-              <div className="col">
-                <h5 className="text-start" style={{ fontSize: "18px" }}>
-                  Morral Bahamas
-                </h5>
-              </div>
-              <div className="col">
-                <h5 className="text-start" style={{ fontSize: "18px" }}>
-                  {" "}
-                  $ 75.000
-                </h5>
-                <p className="" style={{ fontSize: "14px" }}>
-                  <strong>Color</strong>: Negro{" "}
-                </p>
-              </div>
-              <ProductCounter></ProductCounter>
-            </div>
-          </div>
-        </div>
-        <hr
-          className="bg-black opacity-100 w-100"
-          style={{ border: "1px black solid" }}
-        ></hr>
-        <div className="d-flex flex-column px-4">
-          <div className="col">
-            <div className="d-flex">
-              <h8 className="me-auto" style={{ fontSize: "16px" }}>
-                {" "}
-                Subtotal
-              </h8>
-              <h8 className="" style={{ fontSize: "16px" }}>
-                {" "}
-                $ 75.000
-              </h8>
-            </div>
-          </div>
-          <div className="col">
-            <div className="d-flex">
-              <h5 className=" me-auto" style={{ fontSize: "18px" }}>
-                {" "}
-                <strong>Total</strong>
-              </h5>
-
-              <h5 className="" style={{ fontSize: "18px" }}>
-                {" "}
-                <strong>$ 75.000</strong>
-              </h5>
-            </div>
-            <p className="" style={{ fontSize: "14px" }}>
-              <strong>Tasas y envios calculadas en el carrito</strong>
-            </p>
-          </div>
-        </div>
-        <div className="m-auto mb-3 ">
-          <button
-            className="btn btn-info text-light px-5"
-            style={{
-              borderRadius: "10px !important",
-              height: "40px",
-              backgroundColor: "#00A5D0",
-            }}
-          >
-            Avanzar con la compra
-          </button>
-        </div>
-      </div>
+      <CartMenu />
     </>
   );
 }
--- frontend/infusion/pages/Checkout.js
+++ frontend/infusion/pages/Checkout.js
@@ -1,9 +1,89 @@
 import React from "react";
+import AddCartButton from "../components/AddCartButton";
+import ProductCounter from "../components/ProductCounter";
 
 export default function Checkout() {
   return (
     <div>
-      <h1>Checkout</h1>
+      <div className="col col-md-auto d-flex flex-column flex-md-row mb-3 mx-auto">
+        <div
+          className="p-2 bg-light h-75"
+          style={{
+            minWidth: "360px",
+            maxWidth: "460px",
+            width: "90%",
+            maxHeight: "260px",
+            fontSize: "14px",
+          }}
+        >
+          <div className="d-flex px-2">
+            <div className="col text-center">
+              <h5 className="my-3 mx-3" style={{ fontSize: "18px" }}>
+                Resumen de la compra
+              </h5>
+            </div>
+          </div>
+
+          <div className="col">
+            <div className="d-flex">
+              <h8 className="me-auto" style={{ fontSize: "16px" }}>
+                {" "}
+                Subtotal
+              </h8>
+              <h8 className="" style={{ fontSize: "16px" }}>
+                {" "}
+                $ 75.000
+              </h8>
+            </div>
+          </div>
+          <hr
+            className="border border-secondary border-1 opacity-75 m-auto"
+            style={{ maxWidth: "95%" }}
+          />
+          <div className="d-flex">
+            <h5 className=" me-auto" style={{ fontSize: "18px" }}>
+              {" "}
+              <strong>Total</strong>
+            </h5>
+
+            <h5 className="" style={{ fontSize: "18px" }}>
+              {" "}
+              <strong>$ 75.000</strong>
+            </h5>
+          </div>
+          <div
+            className="d-flex justify-content-around flex-column"
+            style={{ minWidth: "300px" }}
+          >
+            <div className="m-auto mb-3 ">
+              <button
+                className="btn btn-info text-light px-5"
+                style={{
+                  borderRadius: "10px !important",
+                  height: "40px",
+                  backgroundColor: "#00A5D0",
+                }}
+              >
+                Avanzar con la compra
+              </button>
+            </div>
+            <div className="m-auto mb-3 ">
+              <button
+                className="btn btn-info text-info px-4"
+                style={{
+                  borderRadius: "10px !important",
+                  height: "40px",
+                  backgroundColor: "transparent",
+                }}
+              >
+                {"<--"} Continuar viendo productos
+              </button>
+            </div>
+          </div>
+        </div>
+
+        {/* aqui iria el otro cajon */}
+      </div>
     </div>
   );
 }
