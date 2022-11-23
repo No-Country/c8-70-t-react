4d845b881bad359f634b0936ae4ddcbf93f49387
Noelia Martínez <noeowo384@gmail.com>
2022-11-23 19:45:14 -0400

    user model

--- backend/src/models/index.ts
+++ backend/src/models/index.ts
@@ -2,4 +2,5 @@ export * from './car.model';
 export * from './category.model';
 export * from './product.model';
 export * from './profile.model';
+export * from './user.model';
 
---
+++ backend/src/models/user.model.ts
@@ -0,0 +1,58 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class User extends Entity {
+  @property({
+    type: 'number',
+    id: true,
+    generated: true,
+  })
+  id?: number;
+
+  @property({
+    type: 'string',
+  })
+  username?: string;
+
+  @property({
+    type: 'string',
+  })
+  email?: string;
+
+  @property({
+    type: 'string',
+  })
+  password?: string;
+
+  @property({
+    type: 'string',
+  })
+  role?: string;
+
+  @property({
+    type: 'array',
+    itemType: 'string',
+  })
+  permissions?: string[];
+
+  @property({
+    type: 'date',
+  })
+  modified?: string;
+
+  @property({
+    type: 'date',
+  })
+  created?: string;
+
+
+  constructor(data?: Partial<User>) {
+    super(data);
+  }
+}
+
+export interface UserRelations {
+  // describe navigational properties here
+}
+
+export type UserWithRelations = User & UserRelations;
