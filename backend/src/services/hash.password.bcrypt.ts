5e791ea5559528308b1323c78f17eef05de11a53
Noelia Martínez <noeowo384@gmail.com>
2022-11-24 04:52:10 -0400

    added hash pasword bcrypt

--- backend/src/services/hash.password.bcrypt.ts
+++ backend/src/services/hash.password.bcrypt.ts
@@ -1,11 +1,16 @@
 import {inject} from '@loopback/core';
-import {genSalt, hash} from 'bcryptjs';
+import {compare, genSalt, hash} from 'bcryptjs';
 
 interface PasswordHasher <T = string> {
   hashPassword(password: T): Promise<T>;
+  comparePassword(providedPassword: T, storedPass: T): Promise<boolean>;
 }
 
 export class BcryptHasher implements PasswordHasher<string>{
+  async comparePassword(providedPassword: string, storedPass: string): Promise<boolean> {
+    const passwordMatched = await compare(providedPassword, storedPass);
+    return passwordMatched;
+  }
   @inject('rounds')
   public readonly rounds: number;
   async hashPassword(password: string){
