300ae2ba96b8fd6302975a32e161daed4ec844be
Noelia Martínez <noeowo384@gmail.com>
2022-11-24 04:53:03 -0400

    verify credentials

---
+++ backend/src/services/user-service.ts
@@ -0,0 +1,46 @@
+import {UserService} from '@loopback/authentication';
+import {inject} from '@loopback/core';
+import {repository} from '@loopback/repository';
+import {HttpErrors} from '@loopback/rest';
+import {securityId, UserProfile} from '@loopback/security';
+import {User} from '../models';
+import {Credentials, UserRepository} from '../repositories';
+import {BcryptHasher} from './hash.password.bcrypt';
+
+export class MyUserService implements UserService<User, Credentials>{
+  constructor(
+    @repository(UserRepository)
+    public userRepository: UserRepository,
+    @inject('service.hasher')
+    public hasher: BcryptHasher
+  ){}
+
+  async verifyCredentials(credentials: Credentials): Promise<User> {
+    const foundUser = await this.userRepository.findOne({
+      where:{
+        email: credentials.email
+      }
+    });
+    if(!foundUser){
+      throw new HttpErrors.NotFound(`user not found with ${credentials.email}`);
+    }
+    const passwordMatched = await this.hasher.comparePassword(credentials.password, foundUser.password);
+    if(!passwordMatched){
+      throw new HttpErrors.Unauthorized('password is not valid');
+    }
+    return foundUser;
+  }
+  convertToUserProfile(user: User): UserProfile {
+    let username = '';
+    if(user.username){
+      username = user.username;
+    }
+    return {
+      [securityId]: `${user.id}`,
+      id: user.id,
+      username: username,
+      email: user.email
+    }
+  }
+
+}
