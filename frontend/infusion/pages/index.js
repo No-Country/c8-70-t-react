4927d0c10019b984b1613eaf0f65a6a7c171dbaf
agtelo <alantelo1987@gmail.com>
2022-12-03 15:08:58 -0300

    add slider

--- frontend/infusion/pages/index.js
+++ frontend/infusion/pages/index.js
@@ -5,12 +5,12 @@ import CategoryList from "../components/CategoryList";
 import ProductList from "../components/ProductList";
 import LayoutMargin from "../components/LayoutMargin";
 import Slider from "../components/Slider";
-
 export default function Home() {
   return (
     <>
       <div className="my-5 py-5">
         {/* <Title title="Nuestros productos son elaborados artesanalmente" /> */}
+        {/* <div className="mx-5 px-5"> */}
         <Slider />
         <LayoutMargin>
           <br />
@@ -39,61 +39,62 @@ export default function Home() {
                   </p>
                 </div>
               </div>
-              <div className="col mt-5">
-                <div className="card border-0">
-                  <button className={`${styles.icon + " " + styles.gratis}`}>
-                    {/* <i className={` bi bi-cart-fill`}></i> */}
-                    <img src="/envios.svg" alt="envios"></img>
-                  </button>
-                  <div className="card-body p-5">
-                    <h5 className="card-title text-uppercase">Envios Gratis</h5>
-                    <p className="card-text">
-                      This is a longer card with supporting text below as a
-                      natural lead-in to additional content. This content is a
-                      little bit longer.
-                    </p>
-                  </div>
-                </div>
-              </div>
-              <div className="col mt-5">
-                <div className="card border-0">
-                  <button className={`${styles.icon + " " + styles.seguro}`}>
-                    {/* <i className={` bi bi-cart-fill`}></i> */}
-                    <img src="/seguro.svg" alt="seguro"></img>
-                  </button>
-                  <div className="card-body p-5">
-                    <h5 className="card-title text-uppercase">Sitio Seguro</h5>
-                    <p className="card-text">
-                      This is a longer card with supporting text below as a
-                      natural lead-in to additional content. This content is a
-                      little bit longer.
-                    </p>
-                  </div>
+            </div>
+            <div className="col mt-5">
+              <div className="card border-0">
+                <button className={`${styles.icon + " " + styles.gratis}`}>
+                  {/* <i className={` bi bi-cart-fill`}></i> */}
+                  <img src="/envios.svg" alt="envios"></img>
+                </button>
+                <div className="card-body p-5">
+                  <h5 className="card-title text-uppercase">Envios Gratis</h5>
+                  <p className="card-text">
+                    This is a longer card with supporting text below as a
+                    natural lead-in to additional content. This content is a
+                    little bit longer.
+                  </p>
                 </div>
               </div>
             </div>
-            <br />
-            <Title
-              title="Los mas populares"
-              subtitle="Lorem ipsum dolor sit, amet consectetur adipisicing elit."
-            />
-            <br />
-            <br />
-            <ProductList />
-            <div className="d-flex justify-content-center">
-              <div className="mt-5 mb-5 justify-content-center btn btn-secondary bg-white w-25">
-                <Link
-                  href="/About"
-                  className="text-decoration-none fs-4 border-dark border-1 text-dark m-2 m-auto"
-                >
-                  {/* {" "} */}
-                  Ver mas
-                </Link>
+            <div className="col mt-5">
+              <div className="card border-0">
+                <button className={`${styles.icon + " " + styles.seguro}`}>
+                  {/* <i className={` bi bi-cart-fill`}></i> */}
+                  <img src="/seguro.svg" alt="seguro"></img>
+                </button>
+                <div className="card-body p-5">
+                  <h5 className="card-title text-uppercase">Sitio Seguro</h5>
+                  <p className="card-text">
+                    This is a longer card with supporting text below as a
+                    natural lead-in to additional content. This content is a
+                    little bit longer.
+                  </p>
+                </div>
               </div>
+            </div>
+          </div>
+          <br />
+          <Title
+            title="Los mas populares"
+            subtitle="Lorem ipsum dolor sit, amet consectetur adipisicing elit."
+          />
+          <br />
+          <br />
+          <ProductList />
+          <div className="d-flex justify-content-center">
+            <div className="mt-5 mb-5 justify-content-center btn btn-secondary bg-white w-25">
+              <Link
+                href="/About"
+                className="text-decoration-none fs-4 border-dark border-1 text-dark m-2 m-auto"
+              >
+                {/* {" "} */}
+                Ver mas
+              </Link>
             </div>
           </div>
         </LayoutMargin>
       </div>
+      {/* </div> */}
     </>
   );
 }
